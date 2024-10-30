06a4ad7ad9dd3a639330293e9904d6610cadf22a
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2024-10-31 00:07:29 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -9,15 +9,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Auth0Provider
+       <Auth0Provider
         domain="dev-h1d4rxroy6bknice.us.auth0.com"
         clientId="fLNpi9UHFdx7HFmGZ3edTYkVipAO8yp7"
         authorizationParams={{
           redirect_uri: window.location.origin
         }}
-      > */}
+      > 
         <App />
-      {/* </Auth0Provider> */}
+       </Auth0Provider> 
     </BrowserRouter>
   </React.StrictMode>
 );
